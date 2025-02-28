334f4813d6fe48442065c9101b6636b5bea0827d
Kaz Piros <117852608+Kazpiros@users.noreply.github.com>
2025-02-28 12:17:04 -0800

    Update and rename One.vhd to Register_File.vhd

--- Register_File.vhd
+++ Register_File.vhd
@@ -2,17 +2,19 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
+--use ieee.numeric_std.all;
 
 entity register_file is
-port(rst : in std_logic; clk: in std_logic;
---read signals
-rd_index1: in std_logic_vector(2 downto 0); 
-rd_index2: in std_logic_vector(2 downto 0); 
-rd_data1: out std_logic_vector(15 downto 0); 
-rd_data2: out std_logic_vector(15 downto 0);
---write signals
-wr_index: in std_logic_vector(2 downto 0); 
-wr_data: in std_logic_vector(15 downto 0); wr_enable: in std_logic);
+port(
+   rst : in std_logic; clk: in std_logic;
+    --read signals
+   rd_index1: in std_logic_vector(2 downto 0); 
+   rd_index2: in std_logic_vector(2 downto 0); 
+   rd_data1: out std_logic_vector(15 downto 0); 
+   rd_data2: out std_logic_vector(15 downto 0);
+   --write signals
+   wr_index: in std_logic_vector(2 downto 0); 
+   wr_data: in std_logic_vector(15 downto 0); wr_enable: in std_logic);
 end register_file;
 
 architecture behavioural of register_file is
@@ -30,12 +32,12 @@ begin
    elsif(wr_enable= '1') then
       case wr_index(2 downto 0) is
         when "000" => reg_file(0) <= wr_data;
-        when "001" => reg_file(0) <= wr_data;
-        when "010" => reg_file(0) <= wr_data;
-        when "011" => reg_file(0) <= wr_data;
-        when "100" => reg_file(0) <= wr_data;
-        when "101" => reg_file(0) <= wr_data;
-        when "110" => reg_file(0) <= wr_data;      
+        when "001" => reg_file(1) <= wr_data;
+        when "010" => reg_file(2) <= wr_data;
+        when "011" => reg_file(3) <= wr_data;
+        when "100" => reg_file(4) <= wr_data;
+        when "101" => reg_file(5) <= wr_data;
+        when "110" => reg_file(6) <= wr_data;      
       --fill this part filled!
       when others => NULL; end case;
     end if; 
