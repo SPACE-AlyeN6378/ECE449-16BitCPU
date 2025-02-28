969f3638212247d8b644d218a154508aa57272aa
Kaz Piros <117852608+Kazpiros@users.noreply.github.com>
2025-02-28 12:18:07 -0800

    Update alu.vhd

--- alu.vhd
+++ alu.vhd
@@ -1,58 +1,77 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 02/10/2025 06:12:54 PM
--- Design Name: 
--- Module Name: alu - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.numeric_std.all;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+library work;
+use work.all;
+--- -fsynopsys -fexplicit
 entity alu is
-port(
-    rst : in std_logic; 
-    clk: in std_logic;
-    
-    alu_mode: in std_logic_vector(2 downto 0);
---read signals
-    rd_data1: in std_logic_vector(15 downto 0); 
-    rd_data2: in std_logic_vector(15 downto 0);
-    
---output signals
-    result: out std_logic_vector(15 downto 0);
-    z_flag: out std_logic;
-    n_flag: out std_logic;
-    
---write signals
+    port (
+        alu_mode : in std_logic_vector(2 downto 0); -- ALU op code from decode stage
+        in1 : in std_logic_vector(15 downto 0); -- Input from RA
+        in2 : in std_logic_vector(15 downto 0); -- Input from RB
+        resultL : out std_logic_vector(15 downto 0); -- Lower 16 bits of result
+        resultU : out std_logic_vector(15 downto 0); -- Upper 16 bits of result
+        z_flag : out std_logic;
+        n_flag : out std_logic
+    );
 end alu;
 
-architecture Behavioral of alu is
-    result <= std_logic_vector(unsigned(pr_in1) + unsigned(pr_in2));
+architecture behavioral of alu is
+    -- 32-bit temporary result for full multiplication and addition overflow
+    signal temp_result: std_logic_vector(31 downto 0);
+    
+    -- Addition with overflow detection
+    signal add_result: std_logic_vector(16 downto 0);  -- 17 bits for overflow
 begin
+    process(alu_mode, in1, in2)
+    --variable mult_result: signed(31 downto 0);
+    begin
+        -- Default values
+        temp_result <= (others => '0');
+        add_result <= (others => '0');
+        
+        case alu_mode is
+            when "000" =>  -- NOP
+                temp_result <= x"00000000";  -- Pass through first input
+                
+            when "001" =>  -- ADD                 MAY CAUSE ERRORS
+                add_result <= std_logic_vector(('0' & signed(in1)) + ('0' & signed(in2)));
+                temp_result <= x"0000" & add_result(15 downto 0); -- Perform 17-bit addition to catch overflow
+                if add_result(16) = '1' then  -- Handle overflow
+                    temp_result(16) <= '1';  -- Set overflow bit in upper result
+                end if;
+                
+            when "010" =>  -- SUB
+                temp_result <= x"0000" & std_logic_vector(signed(in1) - signed(in2));
+                
+            when "011" =>  -- MUL
+                temp_result <= std_logic_vector(signed(in1) * signed(in2));
+                
+            when "100" =>  -- NAND
+                temp_result <= x"0000" & (in1 nand in2);
+                
+            when "101" =>  -- LSL
+                temp_result <= x"0000" & std_logic_vector(shift_left(signed(in1), 
+                             to_integer(signed(in2(3 downto 0))) mod 16));  -- mod added to fix edge case, could be wrong
+                
+            when "110" =>  -- LSR
+                temp_result <= x"0000" & std_logic_vector(shift_right(signed(in1), 
+                             to_integer(signed(in2(3 downto 0))) mod 16));
+                
+            when "111" =>  -- TEST
+                temp_result <= x"0000" & (in1 and in2);
+                
+            when others =>
+                temp_result <= (others => '0');
+        end case;
+    end process;
 
+    -- Set flags based on lower result
+    z_flag <= '1' when temp_result(15 downto 0) = x"0000" else '0';
+    n_flag <= temp_result(15);  -- Sign bit from lower result
+    
+    -- Output the results
+    resultL <= temp_result(15 downto 0);   -- Lower 16 bits
+    resultU <= temp_result(31 downto 16);  -- Upper 16 bits
 
-end Behavioral;
+end behavioral;
